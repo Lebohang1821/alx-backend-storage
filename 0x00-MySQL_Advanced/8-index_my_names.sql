4942fadaffb51b6cb3593b2a9e47dfef5dd9c356
Lebohang1821 <133529043+Lebohang1821@users.noreply.github.com>
2024-04-24 19:23:27 +0200

    Update 8-index_my_names.sql

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,2 +1,2 @@
--- Creates an index idx_name_first on the table names and the first letter of name.
+-- It defines index idx_name_first on table names and first letter of each name
 CREATE INDEX idx_name_first ON names(name(1));
