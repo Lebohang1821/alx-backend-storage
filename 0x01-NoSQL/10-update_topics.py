fce4e1ceb4331f029fc763266f29d9b4cce3889a
Lebohang1821 <133529043+Lebohang1821@users.noreply.github.com>
2024-04-29 18:18:34 +0200

    10

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 '''
-Change school topic
+It changes schools topic
 '''
 
 
 def update_topics(mongo_collection, name, topics):
-  '''changes all topics of a school doc'''
+  '''It changes * topics of schools doc'''
   mongo_collection.update_many(
     {'name': name},
     {'$set': {'topics': topics}}
