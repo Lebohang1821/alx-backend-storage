54224044ca8e00df46ee4f44ec3e2664d6b563cc
Lebohang1821 <133529043+Lebohang1821@users.noreply.github.com>
2024-04-29 18:23:37 +0200

    102

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-""" MongoDB Operations with Python using pymongo """
+""" Improve 12-log_stats.py by adding top 10 of most present IPs """
 from pymongo import MongoClient
 
 if __name__ == "__main__":
-    """Provides some stats about Nginx logs stored in MongoDB"""
+    """It gives some stats about Nginx logs stored in MongoDB"""
     client = MongoClient("mongodb://127.0.0.1:27017")
     nginx_collection = client.logs.nginx
 
